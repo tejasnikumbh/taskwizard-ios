af5f7042e027e253b29f53d1305b1757d08aae44
tejasnikumbh <tejasnikumbh@gmail.com>
2016-05-09 03:36:42 +0100

    Initial Commit

---
+++ taskwizard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  taskwizard
+//
+//  Created by Tejas Nikumbh on 5/9/16.
+//  Copyright © 2016 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
