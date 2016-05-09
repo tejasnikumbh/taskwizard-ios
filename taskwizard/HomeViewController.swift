13bdb6a2c9d91492e0b58d03aa6aced525ca90ed
tejasnikumbh <tejasnikumbh@gmail.com>
2016-05-09 09:02:59 +0100

    Initial VC complete

--- taskwizard/HomeViewController.swift
+++ taskwizard/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  taskwizard
 //
 //  Created by Tejas Nikumbh on 5/9/16.
@@ -8,18 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
