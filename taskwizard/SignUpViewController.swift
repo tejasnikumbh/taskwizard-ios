ceb7d8a9f02be6fd02aeae63309c27646a194b70
tejasnikumbh <tejasnikumbh@gmail.com>
2016-05-18 02:24:27 +0100

    Added alamorfire + signup

---
+++ taskwizard/SignUpViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpViewController.swift
+//  taskwizard
+//
+//  Created by Tejas Nikumbh on 5/18/16.
+//  Copyright © 2016 Personal. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: TWBaseFormViewController {
+
+    @IBAction func backButtonTapped(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
